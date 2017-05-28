74bccb929530a06afc008413bbe6bfa253ac2e02
olbrew <olbrew@users.noreply.github.com>
2017-05-28 22:28:58 +0200

    Update fish environment variables

--- config.fish
+++ config.fish
@@ -11,15 +11,9 @@ set -x EDITOR 'vim'
 set -x ARCHFLAGS '-arch x86_64'
 # Prevent ._* files from being written
 set -x COPYFILE_DISABLE 'true'
-# Homebrew caskroom
-set -x HOMEBREW_CASK_OPTS '--caskroom=/usr/local/caskroom'
 # Let fzf use `rg` instead of `find`
-set -x FZF_DEFAULT_COMMAND 'rg -g ""'
-# todo.txt options:
-set -x TODOTXT_DEFAULT_ACTION 'ls'
+set -x FZF_DEFAULT_COMMAND 'rg -S ""'
 # no insecure redirect for homebrew
 set -x HOMEBREW_NO_INSECURE_REDIRECT 'true'
 # disable homebrew analytics
 set -x HOMEBREW_NO_ANALYTICS 1
-# gopath
-set -x GOPATH $HOME/Developer/go
