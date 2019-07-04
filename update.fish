4efec21aabb4c2e7bf6953b9deb6b6bac99a52c5
olbrew <olbrew@users.noreply.github.com>
2019-07-04 22:24:47 +0200

    Add representative emoji to update script 🤓

--- update.fish
+++ update.fish
@@ -1,38 +1,37 @@
 function update -d "Update all tools and applications om MacOS"
     if nc -z google.com 80  >/dev/null 2>&1
-        echo "🆕  Updating System (root)"
+        echo "🍎 Updating System (root)"
         sudo softwareupdate --install -all
         echo \n
 
-        echo "🆕  Updating Brew"
+        echo "🍺 Updating Brew"
         brew update
         brew upgrade
         brew cleanup
-        brew prune
         echo \n
 
-        echo "🆕  Updating Python"
-        pip list --outdated --local | awk '{print $1;}' | xargs -n1 pip install -U
-        echo \n
+        #echo "🐍 Updating Python"
+        #pip list --outdated --local | awk '{print $1;}' | xargs -n1 pip install -U
+        #echo \n
 
-        echo "🆕  Updating Vim"
+        echo "✏️  Updating NeoVim"
         nvim +PlugUpgrade +PlugUpdate +qall
         echo \n
 
-        echo "🆕  Updating Ruby Gems"
-        gem update
-        echo \n
-
-        echo "🆕  Updating TLDR"
+        echo "📒 Updating TLDR"
         tldr --update
         echo \n
 
-        echo "🆕  Updating Yarn"
+        echo "🧶 Updating Yarn"
         yarn global upgrade
         echo \n
 
-        echo "🆕  Updating Fish Completions"
+        echo "🐡 Updating Fish Completions"
         fish_update_completions
+        echo \n
+
+        echo "📂 Cleaning stale directories from z cache"
+        z --clean
         echo \n
 
         echo "Succes! You are fully up to date now. 👍 "
