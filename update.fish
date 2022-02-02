6fd20a11e884edd763cd021bf1b8f98b4039ffb9
olbrew <olivierbrewaeys@gmail.com>
2022-02-02 17:03:06 +0100

    Updates to update script

--- update.fish
+++ update.fish
@@ -1,4 +1,4 @@
-function update -d "Update all tools and applications om MacOS"
+function update -d "Update all tools and applications on MacOS"
     if nc -z google.com 80  >/dev/null 2>&1
         echo "🍎 Updating System (root)"
         sudo softwareupdate --install -all
@@ -11,28 +11,30 @@ function update -d "Update all tools and applications om MacOS"
         brew cleanup
         echo \n
 
-        #echo "💎 Updating Ruby Gems"
-        #sudo gem update -- --with-openssl-dir=/usr/local/Cellar/openssl@1.1/1.1.1k
-        #echo \n
+        echo "💎 Updating Ruby Gems"
+        sudo gem update
+        echo \n
 
-        #echo "🐍 Updating Python"
-        #pip3 list --outdated --local | awk '{print $1;}' | xargs -n1 pip3 install -U
-        #echo \n
+        echo "🐍 Updating Python"
+        pip list --outdated | awk '{print $1}' | xargs -n1 pip install -U
+        echo \n
 
-        echo "✏️  Updating NeoVim"
+        echo "✏️ Updating NeoVim"
         nvim +PlugUpgrade +PlugUpdate +qall
+        echo \n
+
+        echo "📦 Updating NPM"
+        npm -g update
+        echo \n
+
+        echo "🐡 Updating Fish Plugins and Completions"
+        fundle self-update
+        fundle update
+        fish_update_completions --keep
         echo \n
 
         echo "📒 Updating TLDR"
         tldr --update
-        echo \n
-
-        #echo "🧶 Updating NPM"
-        #yarn global upgrade
-        #echo \n
-
-        echo "🐡 Updating Fish Completions"
-        fish_update_completions --keep
         echo \n
 
         echo "📂 Cleaning stale directories from z cache"
